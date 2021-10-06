428561728b23573cea2c63cd73466ac2bfed422d
portowyi <port1986.86@gmail.com>
2021-10-06 11:28:31 +0300

    Разделение на frontend и backend

--- backend/util/database.js
+++ backend/util/database.js
@@ -3,7 +3,8 @@ const db_conn_params = require('./db.conn.params');
 
 const sequelize = new Sequelize(db_conn_params.database, db_conn_params.user, db_conn_params.password,{
     dialect: "mysql",
-    host: db_conn_params.host
+    host: db_conn_params.host, 
+    port: 3307
 });
 
 module.exports = sequelize;
