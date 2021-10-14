6bab294ffa30765a60b5c7a2e7c6ad225619df35
portowyi <port1986.86@gmail.com>
2021-10-14 18:21:50 +0300

    Пробую reract-bootstrap

---
+++ frontend/src/components/menu/myNavbar.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Navbar from 'react-bootstrap/Navbar';
+
+export default function Navbar(){
+    return(
+        <Navbar bg="light" expand="lg">
+            <Navbar.Brand href="#">Сервис</Navbar.Brand>
+        </Navbar>
+    );
+};
